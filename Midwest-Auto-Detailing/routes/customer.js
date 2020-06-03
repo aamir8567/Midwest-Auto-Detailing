063e1a64245121d7d5ac6b793684be8561540248
Aamir <aamir8567@gmail.com>
2020-06-02 23:27:32 -0400

    Setup password encryption and got login and logout function

--- Midwest-Auto-Detailing/routes/customer.js
+++ Midwest-Auto-Detailing/routes/customer.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bcrypt = require('bcryptjs');
 var router = express.Router();
 
 // ==================================================
@@ -7,6 +8,23 @@ var router = express.Router();
 router.get('/addrecord', function (req, res, next) {
     res.render('customer/addrec');
 });
+
+// ==================================================
+// Route Provide Login Window
+// ==================================================
+router.get('/login', function(req, res, next) {
+	res.render('customer/login', {message: "Please Login"});
+});
+
+// ==================================================
+// Route to logout
+// ==================================================
+router.get('/logout', function(req, res, next) {
+	req.session.customer_id = 0;
+	req.session.custname = "";
+	res.redirect('/');
+});
+
 
 // ==================================================
 // Route to list all records. Display view to list all records
@@ -53,13 +71,20 @@ router.post('/', function (req, res, next) {
 
     let insertquery = "INSERT INTO customer (firstname,middlename,lastname,email,phone,address1,address2,city,state,zip,username,password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    db.query(insertquery, [req.body.firstname, req.body.middlename, req.body.lastname, req.body.email, req.body.phone, req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.zip, req.body.username, req.body.password], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.render('error');
-        } else {
-            res.redirect('/customer');
-        }
+    bcrypt.genSalt(10, (err, salt) => {
+		bcrypt.hash(req.body.password, salt, (err, hash) => {
+			if(err) { res.render('error');}
+
+
+    db.query(insertquery, [req.body.firstname, req.body.middlename, req.body.lastname, req.body.email, req.body.phone, req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.zip, req.body.username, hash], (err, result) => {
+                if (err) {
+                 console.log(err);
+                 res.render('error');
+             } else {
+                   res.redirect('/');
+             }
+            }); 
+        });
     });
 });
 
@@ -87,8 +112,15 @@ router.get('/:recordid/edit', function (req, res, next) {
 // ==================================================
 router.post('/save', function (req, res, next) {
     let updatequery = "UPDATE customer SET firstname = ?, middlename = ?, lastname = ?, email = ?, phone = ?, address1 = ?, address2 = ?, city = ?, state = ?, zip = ?, username = ?, password = ? WHERE customer_id = " + req.body.customer_id;
+//I DID THIS
+    
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(req.body.password, salt, (err, hash) => {
+                 if(err) { res.render('error');}
 
-    db.query(updatequery, [req.body.firstname, req.body.middlename, req.body.lastname, req.body.email, req.body.phone, req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.zip, req.body.username, req.body.password], (err, result) => {
+
+
+    db.query(updatequery, [req.body.firstname, req.body.middlename, req.body.lastname, req.body.email, req.body.phone, req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.zip, req.body.username, hash], (err, result) => {
         if (err) {
             console.log(err);
             res.render('error');
@@ -96,7 +128,7 @@ router.post('/save', function (req, res, next) {
             res.redirect('/customer');
         }
     });
-});
+});});});
 
 // ==================================================
 // Route to delete one specific record.
@@ -115,7 +147,46 @@ router.get('/:recordid/delete', function (req, res, next) {
     });
 });
 
+// ==================================================
+// Route Enable Registration
+// ==================================================
+router.get('/register', function(req, res, next) {
+	res.render('customer/addrec');
+});
 
+
+
+// ==================================================
+// Route Check Login Credentials
+// ==================================================
+router.post('/login', function(req, res, next) {
+    let query = "select customer_id, firstname, lastname, password from customer WHERE username = '" + req.body.username + "'"; 
+    // execute query
+    db.query(query, (err, result) => {
+          if (err) {res.render('error');} 
+          else {
+              if(result[0])
+                  {
+                  // Username was correct. Check if password is correct
+                  bcrypt.compare(req.body.password, result[0].password, function(err, result1) {
+                      if(result1) {
+                          // Password is correct. Set session variables for user.
+                          var custid = result[0].customer_id;
+                          req.session.customer_id = custid;
+                          var custname = result[0].firstname + " "+ result[0].lastname;
+                          req.session.custname = custname;
+                          res.redirect('/');
+                      } else {
+                          // password do not match
+                          res.render('customer/login', {message: "Wrong Password"});
+                      }
+                  });
+                  }
+              else {res.render('customer/login', {message: "Wrong Username"});}
+          } 
+       });
+  });
+  
 
 
 
